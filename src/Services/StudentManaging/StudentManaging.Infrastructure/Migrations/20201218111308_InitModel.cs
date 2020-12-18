0972a8e7928963785adad18859346f296ea767cf
Majid Parsa <MajidKahaki@gmail.com>
2020-12-18 15:03:41 +0330

    Adding database using EF

---
+++ src/Services/StudentManaging/StudentManaging.Infrastructure/Migrations/20201218111308_InitModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity.Migrations.Model;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentManaging.Infrastructure.Migrations
+{
+    public partial class InitModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
