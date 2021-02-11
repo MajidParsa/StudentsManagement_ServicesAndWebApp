4108b4c035e3607abd7b64a665517633b60dfe35
Majid Parsa <MajidKahaki@gmail.com>
2021-02-11 23:23:43 +0330

    change package.json

--- src/Web App/WebSPA/src/index.js
+++ src/Web App/WebSPA/src/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 const root = document.getElementById("main")
-ReactDom.render(<div>First project</div>, root)
+ReactDom.render(<div>First project Test</div>, root)
